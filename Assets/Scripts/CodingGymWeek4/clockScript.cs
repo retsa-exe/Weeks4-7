dc4e3e02510c80e1e871335b38e1c039fdc7a13b
lin62 <asterlin7890@gmail.com>
2025-01-30 15:27:56 -0500

    Add the clock

---
+++ Assets/Scripts/CodingGymWeek4/clockScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clockScript : MonoBehaviour
+{
+    public AudioClip chime;
+    float t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
