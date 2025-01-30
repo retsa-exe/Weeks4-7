a0de0777e83f9cf50b37e3ff7684522d41627802
lin62 <asterlin7890@gmail.com>
2025-01-30 14:22:04 -0500

    Add script for enable and disable

---
+++ Assets/Scripts/EnableDisable.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDisable : MonoBehaviour
+{
+    public SpriteRenderer sr;
+    public EnableDisable script;
+    public GameObject go;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //sr.enabled = false;
+            //script.enabled = false;
+            go.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //sr.enabled = true;
+            //script.enabled = true;
+            go.SetActive(true);
+        }
+    }
+}
