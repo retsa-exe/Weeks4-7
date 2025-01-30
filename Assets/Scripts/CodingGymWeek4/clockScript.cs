a472d929fa39eaa8fecba7659b93c62c5f693edc
lin62 <asterlin7890@gmail.com>
2025-01-30 15:49:07 -0500

    Stop for now

--- Assets/Scripts/CodingGymWeek4/clockScript.cs
+++ Assets/Scripts/CodingGymWeek4/clockScript.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class clockScript : MonoBehaviour
 {
     public AudioClip chime;
-    float t;
+    public float speed = 5f;
+    public float t;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,13 @@ public class clockScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, - speed * Time.deltaTime);
+
+        t += speed * Time.deltaTime;
+
+        if (t > 30)
+        {
+            t = 0;
+        }
     }
 }
