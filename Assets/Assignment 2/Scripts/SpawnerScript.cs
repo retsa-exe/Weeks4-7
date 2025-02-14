ad3d4adc88c23f2e63e9e2f7c2f4a1c7b830a13c
retsa-exe <asterlin7890@gmail.com>
2025-02-14 14:07:36 -0500

    Add spawner script

--- Assets/Assignment 2/Scripts/SpawnerScript.cs
+++ Assets/Assignment 2/Scripts/SpawnerScript.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class SpawnerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //input the prefabs
+    public GameObject paperPrefab;
+    public GameObject scissorPrefab;
+    public GameObject rockPrefab;
+
+    //inputs from the panel
+    public float speed;
+    public float scale;
+
+    //array list for spawned objects
+    public List<GameObject> spawnedObjects;
+
+    private void Start()
     {
-        
+        //new array list for the spawned objects
+        spawnedObjects = new List<GameObject>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //instantiate objects when space down
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //get mouse position
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //instantiate the object at mouse position
+            GameObject newSpawned = Instantiate(paperPrefab, mousePos, Quaternion.identity);
+            //add the new spawned object to list
+            spawnedObjects.Add(newSpawned);
+        }
     }
 }
