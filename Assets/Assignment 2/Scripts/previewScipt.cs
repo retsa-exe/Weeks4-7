97b8a4996ac1a4cfad0c8c00b4d5c470b543f252
retsa-exe <asterlin7890@gmail.com>
2025-02-16 14:41:20 -0500

    Add preview script

---
+++ Assets/Assignment 2/Scripts/previewScipt.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class previewScipt : MonoBehaviour
+{
+    //call slider
+    public Slider slider;
+    //the preview image need to be changed
+    public Image preview;
+
+    private void Update()
+    {
+        //get rect Transform component
+        RectTransform rt = preview.GetComponent<RectTransform>();
+        //change the size with the slider
+        rt.localScale = Vector3.one * slider.value;
+    }
+}
