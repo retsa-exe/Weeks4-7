ab24d8aeba4ddf0cdb7f81dc85df77632ac1726b
retsa-exe <asterlin7890@gmail.com>
2025-02-16 16:25:55 -0500

    Add movement script

---
+++ Assets/Assignment 2/Scripts/movement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movement : MonoBehaviour
+{
+    //get the slider value
+    public Slider slider;
+
+    //object info
+    Vector2 direction;
+    public float speed;
+
+    private void Start()
+    {
+        speed = slider.value;
+        direction = Random.insideUnitCircle;
+    }
+
+    private void Update()
+    {
+        //record current position
+        Vector2 pos = transform.position;
+        //translate screen position
+        Vector2 screenPos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+        //add the direction acroding to the speed and direction
+        pos += direction * speed * Time.deltaTime;
+
+        //make the object bounce in the screen
+        if (pos.x < -screenPos.x)
+        {
+            //reset the value
+            pos.x = -screenPos.x;
+            //reverse the direction
+            direction.x *= -1;
+        }
+        if (pos.x > screenPos.x)
+        {
+            pos.x = screenPos.x;
+            direction.x *= -1;
+        }
+        if (pos.y > screenPos.y)
+        {
+            pos.y = screenPos.y;
+            direction.y *= -1;
+        }
+        if (pos.y < -screenPos.y)
+        {
+            pos.y = -screenPos.y;
+            direction.y *= -1;
+        }
+
+        //update object position
+        transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+    }
+}
---
+++ Assets/Assignment 2/Scripts/paperScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paperScript : MonoBehaviour
+{
+    //object info
+    public float scale;
+    public float speed;
+
+    private void Start()
+    {
+        
+    }
+}
