5e0cc950fa7bfc6989cfff59da13a60dcdf96377
retsa-exe <asterlin7890@gmail.com>
2025-02-16 16:50:51 -0500

    Add removeWhenClicked script

---
+++ Assets/Assignment 2/Scripts/removeWhenClicked.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class removeWhenClicked : MonoBehaviour
+{
+    //call spawner for the lists
+    public SpawnerScript spawner;
+    //call the sprite renderer for contain checks
+    public SpriteRenderer sr;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //get mouse position
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (sr.bounds.Contains(mousePos))
+            {
+                //destroy object when clicked
+                Destroy(gameObject);
+                //remove from the list
+            }
+        }
+    }
+}
