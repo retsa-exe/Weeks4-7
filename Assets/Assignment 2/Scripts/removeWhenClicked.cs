f3a85991351448f55c5340a8aee2c23904680f06
retsa-exe <asterlin7890@gmail.com>
2025-02-17 21:19:17 -0500

    Add disappear animation

--- Assets/Assignment 2/Scripts/removeWhenClicked.cs
+++ Assets/Assignment 2/Scripts/removeWhenClicked.cs
@@ -7,6 +7,14 @@ public class removeWhenClicked : MonoBehaviour
     //call the sprite renderer for contain checks
     public SpriteRenderer sr;
 
+    //boolean to check if is time to play the animation
+    public bool isDead;
+    float t; //timer variable
+
+    //animation curve for scale and transparency
+    public AnimationCurve scaleCurve;
+    public AnimationCurve colorCurve;
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -16,8 +24,21 @@ public class removeWhenClicked : MonoBehaviour
             if (sr.bounds.Contains(mousePos))
             {
                 //destroy object when clicked
-                Destroy(gameObject);
+                isDead = true;
+                Destroy(gameObject, 1); //the animation lasts for 1 sec
             }
+        }
+
+        if (isDead)
+        {
+            t += Time.deltaTime / 30;
+            //scale the object 
+            transform.localScale = transform.localScale * scaleCurve.Evaluate(t);
+
+            //change the transparncy
+            Color currentColor = sr.color;
+            currentColor.a = colorCurve.Evaluate(t * 20);
+            sr.color = currentColor;
         }
     }
 }
